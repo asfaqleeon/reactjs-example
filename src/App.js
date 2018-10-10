270ae3e2e23e140d5f677b238e219c56c7cb862a
asfaqleeon <asfaqleeon@gmail.com>
2018-10-10 11:10:59 +0600

    implement react routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter,Route,Switch,NavLink} from 'react-router-dom';
+//import logo from './logo.svg';
 import './App.css';
+
 
 import List from './components/list-example/List';
 import Table from './components/table-example/Table';
@@ -8,19 +10,65 @@ import Buttons from './components/events-example/Buttons';
 import SingleInput from './components/events-example/SingleInput';
 import MultipleInput from './components/events-example/MultipleInput';
 import ConditionalRender from './components/conditional-render';
-import AxiosGet from './components/axios-get'
+import AxiosGet from './components/axios-get';
+import Error from './error'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/* <List /> */}
-        <Table/>
-        <Buttons />
-        <SingleInput />
-        <MultipleInput />
-        <ConditionalRender />
-        <AxiosGet />
+      <div className="container">
+        
+          <BrowserRouter>
+            <div className="row">
+              <div className="col-2">
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    <NavLink to="/list">lists</NavLink>
+                  </li>
+
+                  <li className="list-group-item">
+                    <NavLink to="/table">table</NavLink>
+                  </li>
+
+                  <li className="list-group-item">
+                    <NavLink to="/buttons">button</NavLink>
+                  </li>
+
+                  <li className="list-group-item">
+                    <NavLink to="/single-input">single input</NavLink>
+                  </li>
+
+                  <li className="list-group-item">
+                    <NavLink to="/multiple-input">multiple input</NavLink>
+                  </li>
+
+                  <li className="list-group-item">
+                    <NavLink to="/conditional-render">conditional render</NavLink>
+                  </li>
+
+                  <li className="list-group-item">
+                    <NavLink to="/axios-get">axios get</NavLink>
+                  </li>
+                </ul>  
+              </div>
+
+              <div className="col-6 main">
+                <Switch>
+                  <Route path="/" component={List} exact /> {/* without exact all others take list component*/}
+                  <Route path="/list" component={List} />
+                  <Route path="/table" component={Table} />
+                  <Route path="/buttons" component={Buttons} />
+                  <Route path="/single-input" component={SingleInput} />
+                  <Route path="/multiple-input" component={MultipleInput} />
+                  <Route path="/conditional-render" component={ConditionalRender} />
+                  <Route path="/axios-get" component={AxiosGet} />
+                  <Route component={Error} />
+                </Switch>
+              </div>
+            </div>
+            
+          </BrowserRouter>
+        
       </div>
     );
   }
---
+++ src/error.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Error = () => {
+    return (
+        <div className="alert alert-danger">
+            invalid url
+        </div>
+    );
+}
+
+export default Error;
